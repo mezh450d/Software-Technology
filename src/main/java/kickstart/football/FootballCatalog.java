56c3b58afe9cacd24440a801ae7829f11ffe5ec4
xXNicksdaXx <74011668+xXNicksdaXx@users.noreply.github.com>
2021-11-10 13:57:27 +0100

    Fußball: Datenbank aktiviert

--- src/main/java/kickstart/football/FootballCatalog.java
+++ src/main/java/kickstart/football/FootballCatalog.java
@@ -18,11 +18,6 @@ package kickstart.football;
 import org.salespointframework.catalog.Catalog;
 import org.springframework.data.domain.Sort;
 
-/**
- * An extension of {@link Catalog} to add video shop specific query methods.
- *
- * @author Oliver Gierke
- */
 public interface FootballCatalog extends Catalog<FootballMatch> {
 
 	Sort DEFAULT_SORT = Sort.by("productIdentifier").descending();
