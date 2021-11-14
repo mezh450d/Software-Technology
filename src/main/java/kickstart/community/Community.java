751e9a84381c4563af8d00d7b3d65667ee9067dd
jing zhou <jing.zhou@mailbox.tu-dresden.de>
2021-11-14 15:14:05 +0100

    Loggedin user kann nicht richtig beitreten.

--- src/main/java/kickstart/community/Community.java
+++ src/main/java/kickstart/community/Community.java
@@ -1,10 +1,15 @@
 package kickstart.community;
 
 import kickstart.lottery.user.User;
+import net.bytebuddy.asm.Advice;
 import org.salespointframework.useraccount.Password;
 import org.salespointframework.useraccount.UserAccount;
+import org.springframework.security.config.annotation.authentication.configurers.ldap.LdapAuthenticationProviderConfigurer;
+import org.springframework.security.config.authentication.PasswordEncoderParser;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.*;
+import java.net.PasswordAuthentication;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -16,32 +21,41 @@ public class Community {
 
 	private String name;
 
-	private Password.EncryptedPassword password;
+	private String password;
+
+	//private Password.EncryptedPassword password;
+
+	//private UserAccount communityAccount;
 
 	@OneToMany(cascade = CascadeType.ALL)
 	private List<User>users =new ArrayList<>();
 
+	@SuppressWarnings("unused")
 	public Community(){}
 
-	public Community(String name, org.salespointframework.useraccount.Password.EncryptedPassword password){
+	public Community(String name, String password){
 		this.name=name;
 		this.password=password;
 	}
 
-	public Community(UserAccount communityAccount) {
-		this.name=communityAccount.getUsername();
-		this.password=communityAccount.getPassword();
-	}
+//	public Community(UserAccount communityAccount) {
+//		this.name=communityAccount.getUsername();
+//		this.password=communityAccount.getPassword();
+//
+//	}
 
 	public long getId() {
 		return id;
 	}
 
+	//public UserAccount getCommunityAccount(){return communityAccount;}
+
 	public List<User> getUsers() {
 		return users;
 	}
 
-	public Password.EncryptedPassword getPassword(){
+
+	public String getPassword(){
 		return password;
 	}
 
--- src/main/java/kickstart/community/CommunityRepository.java
+++ src/main/java/kickstart/community/CommunityRepository.java
@@ -1,10 +1,14 @@
 package kickstart.community;
 
 import kickstart.lottery.user.User;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.util.Streamable;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommunityRepository extends CrudRepository<Community, Long> {
 	@Override
 	Streamable<Community> findAll();
+//	Streamable<Community> findByCreateForm(CreateForm createForm, Sort sort);
 }
