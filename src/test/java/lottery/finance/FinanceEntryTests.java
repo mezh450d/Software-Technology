09e0f978b72f9cecb2186c5527b9135958c5b603
Nick <nick.ruider@gmail.com>
2021-11-24 21:25:29 +0100

    fixes um Finanzen - Betting zu verknüpfen

--- src/test/java/lottery/finance/FinanceEntryTests.java
+++ src/test/java/lottery/finance/FinanceEntryTests.java
@@ -5,12 +5,12 @@ import lottery.user.User;
 import lottery.user.UserManagement;
 import lottery.user.UserRepository;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.salespointframework.useraccount.UserAccount;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.util.Streamable;
-import org.testng.annotations.Test;
 import java.time.LocalDateTime;
 import java.util.List;
 
