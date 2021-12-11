45f66192971ec02b3e62cb007a33bc1821fca449
HannesReinhold <hannes.reinhold2001@gmail.com>
2021-12-11 22:47:48 +0100

    Nutzerdaten Ändern + Validieren Hinzugefügt
    Nutzer kann folgende Daten ändern / hinzufügen:
    ->Vorname
    ->Nachname
    ->Email-Adresse
    ->Lotterie-Adresse

--- src/main/java/lottery/user/UserController.java
+++ src/main/java/lottery/user/UserController.java
@@ -2,6 +2,8 @@ package lottery.user;
 
 import  javax.validation.Valid;
 
+import org.salespointframework.useraccount.UserAccount;
+import org.salespointframework.useraccount.web.LoggedIn;
 import org.springframework.data.util.Streamable;
 import org.springframework.ui.Model;
 import org.springframework.util.Assert;
@@ -9,8 +11,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -60,6 +64,21 @@ public class UserController {
 		return "redirect:/login";
 	}
 
+	@PostMapping("/edit_user")
+	String editCurrentUser(@Valid @ModelAttribute("form") UserEditForm form, Errors errors, Model model,
+						   @RequestParam(value = "action") String action, @LoggedIn UserAccount user) {
+
+
+		//Falls das Formular Fehler enthält, keinen User erstellen
+		if (errors.hasErrors()) {
+			return edit_user(user, model, form);
+		}
+
+		userManagement.editUser(user, form);
+		return "redirect:/home";
+	}
+
+
 	@GetMapping("/register")
 	String register(Model model, RegistrationForm form) {
 		return "register";
@@ -74,5 +93,17 @@ public class UserController {
 	public String login() {
 		return "login";
 	}
+
+	@GetMapping("/edit_user")
+	public String edit_user(@LoggedIn UserAccount user, Model model, UserEditForm form) {
+
+		model.addAttribute("firstName", user.getFirstname());
+		model.addAttribute("lastName", user.getLastname());
+		model.addAttribute("emailAddress", user.getEmail());
+		model.addAttribute("lotteryAddress", userManagement.findByUserAccount(user).getLotteryAddress());
+		model.addAttribute("form", form);
+		return "edit_user";
+	}
+
 
 }
